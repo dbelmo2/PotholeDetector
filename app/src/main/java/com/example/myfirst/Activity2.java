2117a5115f12f95368afd36b104475c2145a79e0
sacost6 <sacost6@uic.edu>
2019-08-08 16:46:58 -0500

    Added second activity for menu

---
+++ app/src/main/java/com/example/myfirst/Activity2.java
@@ -0,0 +1,61 @@
+package com.example.myfirst;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.QuickContactBadge;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.view.Menu;
+
+import com.example.myfirst.android.app.Accelerometer;
+import com.example.myfirst.android.app.Coordinate;
+import com.example.myfirst.android.app.DataBaseHelper;
+import com.example.myfirst.android.app.GPS;
+import com.example.myfirst.android.app.Search;
+
+import java.io.Serializable;
+
+public class Activity2 extends AppCompatActivity implements Serializable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        getSupportActionBar().setTitle("Settings");
+
+        /* add event listener for enter button in threshold setting */
+
+        Button enter = (Button) findViewById(R.id.enter);
+        final EditText userInput = (EditText) findViewById(R.id.userInput);
+        enter.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                TextView mText = (TextView) findViewById(R.id.textView1);
+                mText.setText(userInput.getText().toString());
+                String thresholdStr = userInput.getText().toString();
+                Float threshold = Float.valueOf(thresholdStr);
+                Intent intent = new Intent(Activity2.this, MainActivity.class);
+                intent.putExtra("newThreshold", threshold);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
--- app/src/main/java/com/example/myfirst/MainActivity.java
+++ app/src/main/java/com/example/myfirst/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.myfirst;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.database.Cursor;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.QuickContactBadge;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.view.Menu;
 
 import com.example.myfirst.android.app.Accelerometer;
 import com.example.myfirst.android.app.Coordinate;
@@ -23,11 +27,13 @@ import com.example.myfirst.android.app.DataBaseHelper;
 import com.example.myfirst.android.app.GPS;
 import com.example.myfirst.android.app.Search;
 
+import java.io.Serializable;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements Serializable {
 
     private LocationListener locationListener;
-    private Button viewData, start, updateThresh, addCoordinate;
+    private Button viewData, start, updateThresh, addCoordinate, settings;
     private TextView threshText;
     private SeekBar seekBar;
     private LocationManager locationManager;
@@ -38,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     private Search search;
     private MediaPlayer mediaPlayer;
     private EditText threshold, latInput, longInput;
+    private Toolbar toolbar;
 
 
 
@@ -56,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
         search = new Search(database, gps, accelerometer, mediaPlayer);
 
 
-
         // get the button from the activity view, using the ID: rollButton
         threshText = findViewById(R.id.threshText);
         start = findViewById(R.id.Startbutton);
@@ -66,24 +72,51 @@ public class MainActivity extends AppCompatActivity {
         longInput = findViewById(R.id.long_input);
         updateThresh = findViewById(R.id.update);
         addCoordinate = findViewById(R.id.add_coordinate);
+        settings = (Button) findViewById(R.id.SettingsButton);
 
+        /* Adding event listener for SETTING button */
 
-
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettingsActivity();
+            }
+        });
         //CLEAR DATABASE FOR TESTING PURPOSES
         clearData();
-
 
         search.start();
 
         // setup the background image
         //mainBackground.setImageResource(R.drawable.app2);
-
         // setting up the location manager
-
-
         configureButton();
         viewAll();
 
+        // Check for threshold change
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                //do nothing
+            } else {
+                float temp = extras.getFloat("newThreshold");
+                accelerometer.setTresh(temp);
+                threshText.setText(Float.toString(accelerometer.getThresh()));
+            }
+        } else {
+            accelerometer.setTresh((Float) savedInstanceState.getSerializable("newThreshold"));
+            threshText.setText(Float.toString(accelerometer.getThresh()));
+        }
+
+
+    }
+
+    /* Method that calls the settings menu screen when button is clicked on */
+
+    public void openSettingsActivity() {
+        Intent intent = new Intent(this, Activity2.class);
+        intent.putExtra("accelerometerValue", 40);
+        startActivity(intent);
     }
 
     // method which setups the view button
@@ -177,7 +210,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
 
 
 }
